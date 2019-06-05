04265cb124ab30cc0b9d4afa56e1ac295820f6a5
ferryrules <ferris@ferryrules.com>
2019-06-05 08:08:17 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+  focus = (e) => {
+    console.log("Good!");
+  }
+  blur = (e) => {
+    console.log("Hey! Eyes on me!");
+  }
+
+  render() {
+    return (
+      <button onFocus={this.focus} onBlur={this.blur}></button>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+  handleKeyUp = (e) => {
+    console.log("Entering password...")
+  }
+
+  render() {
+    return (
+      <div className="cell">
+        <input type="password" onKeyUp={this.handleKeyUp} />
+      </div>
+    )
+  }
+}
